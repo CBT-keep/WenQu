11b8aaf0ea891ada62adcc608964c4bbee610cb3
CBT-keep <2076113922@qq.com>
2026-08-14 13:25:58 +0800

    初始工程框架

---
+++ WenQU/src/main/java/com/xia/wenqu/WenQuApplication.java
@@ -0,0 +1,13 @@
+package com.xia.wenqu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WenQuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WenQuApplication.class, args);
+    }
+
+}
---
+++ WenQU/src/test/java/com/xia/wenqu/WenQuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xia.wenqu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WenQuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
